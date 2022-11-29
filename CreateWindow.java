fd2b0b4092dde8ff50760a2edf670991207eb56d
Lucas Bateson <lucas.bateson@gmail.com>
2022-11-29 19:21:58 +0100

    Frame update

--- CreateWindow.java
+++ CreateWindow.java
@@ -1,11 +1,22 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class CreateWindow {
     public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        // print
+        System.out.println(width);
+        System.out.println(height);
         JFrame frame = new JFrame();
-        frame.setSize(150, 150);
+        frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Game");
         frame.setVisible(true);
+
+        while (true) {
+            frame.repaint();
+        }
     }
 }
